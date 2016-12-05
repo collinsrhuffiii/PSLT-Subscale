d7f73464ac269db0d83fdd76c32f6de91fb52fe4
Alex Oxford <alexoxford@users.noreply.github.com>
2016-12-05 15:58:30 -0500

    Update ReadGPS.py

--- ReadGPS.py
+++ ReadGPS.py
@@ -5,23 +5,29 @@ import time
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-while True:
-	report = session.next()
-	dataFile = open("GPSData.csv", "a")
-	data = "\n"
-	if report["class"] == "TPV":
-		if hasattr(report, "lat"):
-			data += str(report.lat) + ","
-		if hasattr(report, "lon"):
-			data += str(report.lon) + ","
-		if hasattr(report, "climb"):
-			data += str(report.climb) + ","
-		if hasattr(report, "altitude"):
-			data += str(report.altitude) + ","
-		if hasattr(report, "speed"):
-			data += str(report.speed) + ","
-		if hasattr(report, "time"):
-			data += str(report.time)
-		dataFile.write(data)
-		
-	time.sleep(1)
+FREQ = float(open("/home/pi/Desktop/PSLT-Subscale/FREQ.txt", 'r').read())
+quit = False
+
+while(not quit):
+	try:
+		report = session.next()
+		dataFile = open("GPSData.csv", "a")
+		data = "\n"
+		if report["class"] == "TPV":
+			if hasattr(report, "lat"):
+				data += str(report.lat) + ","
+			if hasattr(report, "lon"):
+				data += str(report.lon) + ","
+			if hasattr(report, "climb"):
+				data += str(report.climb) + ","
+			if hasattr(report, "altitude"):
+				data += str(report.altitude) + ","
+			if hasattr(report, "speed"):
+				data += str(report.speed) + ","
+			if hasattr(report, "time"):
+				data += str(report.time)
+			dataFile.write(data)
+
+		time.sleep(1.0 / FREQ)
+	except KeyboardInterrupt:
+		quit = True
