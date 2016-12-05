a2073f486f8e9cb8cffeea024041be54f9ed68c9
Alex Oxford <alexoxford@users.noreply.github.com>
2016-12-05 16:51:38 -0500

    Update ReadAccGyro.py

--- lsm/ReadAccGyro.py
+++ lsm/ReadAccGyro.py
@@ -9,20 +9,26 @@ imu.enable()
 imu.calibrateGyroAngles()
 
 start = datetime.now()
+quit = False
+FREQ = float(open("/home/pi/Desktop/PSLT-Subscale/FREQ.txt", 'r').read())
 
-while True:
-	dataFile = open("AccGyroData.csv", "a")
-	data = "\n"
-	stop = datetime.now() - start
-	start = datetime.now()
-	deltaT = stop.microseconds/1000000.0
+while(not quit):
+	try:
+		dataFile = open("AccGyroData.csv", "a")
+		data = "\n"
+		stop = datetime.now() - start
+		start = datetime.now()
+		deltaT = stop.microseconds/1000000.0
+
+		accList = imu.getAccelerometerAngles()
+		data += str(accList[0]) + "," + str(accList[1]) + "," + str(accList[2])
+		gyroList = imu.trackGyroAngles(deltaT = deltaT)
+		data += "," + str(gyroList[0]) + "," + str(gyroList[1]) + "," +str(gyroList[2])
+		dataFile.write(data)
+		dataFile.close()
+		sleep(1.0 / FREQ)
 	
-	accList = imu.getAccelerometerAngles()
-	data += str(accList[0]) + "," + str(accList[1]) + "," + str(accList[2])
-	gyroList = imu.trackGyroAngles(deltaT = deltaT)
-	data += "," + str(gyroList[0]) + "," + str(gyroList[1]) + "," +str(gyroList[2])
-	dataFile.write(data)
-	dataFile.close()
-	sleep(.5)
+	except KeyboardInterrupt:
+		quit = True
 
 
