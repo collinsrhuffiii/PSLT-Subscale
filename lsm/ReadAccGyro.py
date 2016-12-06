b01dc9404eafe73edef80233a234843db80af6da
Alex Oxford <alexoxford@users.noreply.github.com>
2016-12-06 12:48:09 -0500

    Add files via upload

--- lsm/ReadAccGyro.py
+++ lsm/ReadAccGyro.py
@@ -3,10 +3,10 @@ from time import sleep
 import csv
 from altimu import AltIMU
 
+print "ReadAccGyro.py is starting"
 
 imu = AltIMU()
-imu.enable()
-imu.calibrateGyroAngles()
+imu.enable(True, False, True, False, False)
 
 start = datetime.now()
 quit = False
@@ -14,21 +14,26 @@ FREQ = float(open("/home/pi/Desktop/PSLT-Subscale/FREQ.txt", 'r').read())
 
 while(not quit):
 	try:
-		dataFile = open("AccGyroData.csv", "a")
+		dataFile = open("/home/pi/Desktop/PSLT-Subscale/Data/AccGyroData.csv", "a")
 		data = "\n"
 		stop = datetime.now() - start
 		start = datetime.now()
 		deltaT = stop.microseconds/1000000.0
 
-		accList = imu.getAccelerometerAngles()
-		data += str(accList[0]) + "," + str(accList[1]) + "," + str(accList[2])
+		accList = imu.getAccelerometerRaw()
+		data += str(accList[0] / 8000.0) + "," + str(accList[1] / 8000.0) + "," + str(accList[2] / 8000.0)
+		
 		gyroList = imu.trackGyroAngles(deltaT = deltaT)
 		data += "," + str(gyroList[0]) + "," + str(gyroList[1]) + "," +str(gyroList[2])
+		
+		data += "," + str(datetime.now())
+		
 		dataFile.write(data)
 		dataFile.close()
+		
 		sleep(1.0 / FREQ)
 	
 	except KeyboardInterrupt:
 		quit = True
 
-
+print "ReadAccGyro.py is ending"
--- lsm/ReadAccGyro2.py
+++ lsm/ReadAccGyro2.py
@@ -2,50 +2,66 @@ from datetime import datetime
 import time
 import csv
 from altimu import AltIMU
+import RPi.GPIO as GPIO
 
+print "ReadAccGyro2.py is starting"
 
 imu = AltIMU()
-imu.enable()
-imu.calibrateGyroAngles()
+imu.enable(True, False, True, False, False)
 
 start = datetime.now()
 launched = False
 launchTime = -1
 motorState = 0
-FREQ = 2
+quit = False
+FREQ = float(open("/home/pi/Desktop/PSLT-Subscale/FREQ.txt", 'r').read())
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup([21,23], GPIO.OUT)
+p = GPIO.PWM(23, 5120)
 
-while True:
-	dataFile = open("AccGyroData2.csv", "a")
-	data = "\n"
-	stop = datetime.now() - start
-	start = datetime.now()
-	deltaT = stop.microseconds/1000000.0
-	
-	accList = imu.getAccelerometerAngles()
-	data += str(accList[0]) + "," + str(accList[1]) + "," + str(accList[2])
-	gyroList = imu.trackGyroAngles(deltaT = deltaT)
-	data += "," + str(gyroList[0]) + "," + str(gyroList[1]) + "," +str(gyroList[2])
-	dataFile.write(data)
-	dataFile.close()
-	
-	
-	if((acc	List[1] / 8000.0) > 2 & launchTime == -1):
-		launchTime = time.time()
-		launched = True
-	
-	timeElapsed = time.time() - launchTime
-	if(timeElapsed > 4 & motorState == 0 & launched):
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup([21,23], GPIO.OUT)
-		GPIO.output(21,1)
-		p = GPIO.PWM(23, 5120)
-		p.start(100)
-		motorState = 1
-	
-	if(timeElapsed > 6 & motorState == 1):
-		p.stop()
-		GPIO.output(21,0)
-		GPIO.cleanup()
-		motorState = 2
+while(not quit):
+	try:
+		dataFile = open("/home/pi/Desktop/PSLT-Subscale/Data/AccGyroData.csv", "a")
+		data = "\n"
+		stop = datetime.now() - start
+		start = datetime.now()
+		deltaT = stop.microseconds/1000000.0
+
+		accList = imu.getAccelerometerRaw()
+		data += str(accList[0] / 8000.0) + "," + str(accList[1] / 8000.0) + "," + str(accList[2] / 8000.0)
+
+		gyroList = imu.trackGyroAngles(deltaT = deltaT)
+		data += "," + str(gyroList[0]) + "," + str(gyroList[1]) + "," +str(gyroList[2])
+
+		data += "," + str(datetime.now())
+
+		dataFile.write(data)
+		dataFile.close()
+
+
+		if((abs(accList[1] / 8000.0) > 2) & (launchTime == -1)):
+			launchTime = time.time()
+			launched = True
+			print "Launched!"
+
+		timeElapsed = time.time() - launchTime
+		#print "Time elapsed: " + str(timeElapsed)
+		if((timeElapsed > 4) & (motorState == 0) & launched):
+			GPIO.output(21,1)
+			p.start(100)
+			motorState = 1
+			print "Starting motor"
+
+		if((timeElapsed > 6) & (motorState == 1)):
+			p.stop()
+			GPIO.output(21,0)
+			motorState = 2
+			print "Stopping motor"
+
+		time.sleep(1.0 / FREQ)
 		
-	time.sleep(1.0 / FREQ)
+	except KeyboardInterrupt:
+		quit = True
+
+GPIO.cleanup()		
+print "ReadAccGyro2.py is ending"
