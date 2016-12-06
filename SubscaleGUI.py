1ba6377e1053c388b15358f67ed5421f3a6a4cf0
Alex Oxford <alexoxford@users.noreply.github.com>
2016-12-06 15:58:25 -0500

    changed column numbers to read the correct data

--- SubscaleGUI.py
+++ SubscaleGUI.py
@@ -26,13 +26,13 @@ win.setWindowTitle('PSLT Subscale')
 
 pg.setConfigOptions(antialias=True)
 
-alt = np.array(df[df.columns[3]].values)
+alt = np.array(df[df.columns[4]].values) * 3.2808
 p1 = win.addPlot(title="Altitude", y = alt)
 curve1 = p1.plot(pen='y')
 
 
 
-vel = np.array(df[df.columns[2]].values)
+vel = np.array(df[df.columns[3]].values) * 3.2808
 p2 = win.addPlot(title="Velocity", y = vel)
 curve2 = p2.plot(pen='c')
 
@@ -40,7 +40,7 @@ curve2 = p2.plot(pen='c')
 
 win.nextRow()
 
-acc = np.array(df[df.columns[1]].values)
+acc = np.array(df[df.columns[8]].values)
 p3 = win.addPlot(title="Acceleration")
 curve3 = p3.plot(pen='m', y = acc)
 
@@ -48,23 +48,27 @@ curve3 = p3.plot(pen='m', y = acc)
 
 
 p4 = win.addPlot(title="Lat, lon")
-curve4 = p4.plot(pen='y')
+lat = np.array(df[df.columns[1]].values)
+lon = np.array(df[df.columns[2]].values)
+curve4 = p4.plot(pen='r', x = lon, y = lat)
 
 def update():
 	global curve, curve2, data, ptr, p1, p2
 	df = pd.read_csv("exData.csv")
-	alt = np.array(df[df.columns[3]].values)
-	vel = np.array(df[df.columns[2]].values)
-	acc = np.array(df[df.columns[1]].values)
+	alt = np.array(df[df.columns[4]].values) * 3.2808
+	vel = np.array(df[df.columns[3]].values) * 3.2808
+	acc = np.array(df[df.columns[8]].values)
+	lat = np.array(df[df.columns[1]].values)
+	lon = np.array(df[df.columns[2]].values)
 	curve1.setData(alt)
 	curve2.setData(vel)
 	curve3.setData(acc)
+	curve4.setData(lon, lat)
 	
      
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
 timer.start(50)
-
 
 
 
