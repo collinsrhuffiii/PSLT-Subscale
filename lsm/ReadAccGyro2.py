6ae5f8b03f3d491fa342a245bb2dde86ca9f6b46
Alex Oxford <alex@oakhaven.org>
2017-01-11 14:39:20 -0500

    Fixed data file name

--- lsm/ReadAccGyro2.py
+++ lsm/ReadAccGyro2.py
@@ -29,7 +29,7 @@ errs = 0
 while(not quit):
 	try:
 		errs = 0
-		with open("/home/pi/Desktop/PSLT-Subscale/Data/AccGyroData.csv", "ab") as dataFile:	
+		with open("/home/pi/Desktop/PSLT-Subscale/Data/AccGyroData2.csv", "ab") as dataFile:	
 			data = "\n"
 
 			accList = imu.getAccelerometerRaw()
